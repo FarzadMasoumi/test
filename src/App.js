1c1bd366eddb72692891f00a01ac8b813b6b5a0d
unknown <exort1398@gmail.com>
2022-09-14 21:50:32 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
+import SingleQuestion from './Questions';
+import data from './data';
 
 function App() {
+  const [questions, setQuestions] = useState(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h3>سوالی برایتان پیش آمده؟</h3>
+        <div className="info">
+          {
+            questions.map((question) => {
+              return <SingleQuestion key={questions.id}  {...question} />
+            })
+          }
+        </div>
     </div>
   );
 }
